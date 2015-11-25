fc78d2872e34e8b5ee7438f656e79916a7b9301d
David Castillo <castillobgr@gmail.com>
2015-11-25 17:02:38 -0500

    Progress.

---
+++ README.md
@@ -0,0 +1,13 @@
+# gipher
+A GIF generator made with Go.
+
+## Get it
+
+```sh
+$ go get github.com/castillobg/gipher
+```
+
+# To-dos
+
+- User-specified delay.
+- Choose between recursive/ not recursive.
--- main.go
+++ main.go
@@ -2,36 +2,99 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color/palette"
+	"image/draw"
+	"image/gif"
+	_ "image/jpeg"
+	_ "image/png"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
+var delay = 1
+
 func main() {
-	args := os.Args[1:]
+	if len(os.Args) < 3 {
+		showUsage()
+		return
+	}
+	args := os.Args[1 : len(os.Args)-1]
+	outPath := os.Args[len(os.Args)-1]
+	var fileNames []string
 	for _, arg := range args {
-		files, err := getFilesRec(arg)
+		var err error
+		fileNames, err = getImageFileNamesRec(arg)
 		if err != nil {
 			panic(err)
 		}
-		for _, file := range files {
-			fmt.Println(file.Name())
-		}
+	}
+	err := generateGIF(fileNames, outPath)
+	if err != nil {
+		panic(err)
 	}
 }
 
-func getFilesRec(dirName string) ([]os.FileInfo, error) {
+func getImageFileNamesRec(dirName string) ([]string, error) {
 	files, err := ioutil.ReadDir(dirName)
+	var fileNames []string
 	if err != nil {
 		return nil, err
 	}
 	for _, file := range files {
 		if file.IsDir() {
-			childrenFiles, err := getFilesRec(dirName + string(os.PathSeparator) + file.Name())
+			childrenFiles, err := getImageFileNamesRec(filepath.Join(dirName, file.Name()))
 			if err != nil {
 				return nil, err
 			}
-			files = append(files, childrenFiles...)
+			fileNames = append(fileNames, childrenFiles...)
+		} else if formatSupported(file.Name()) {
+			fileNames = append(fileNames, filepath.Join(dirName, file.Name()))
 		}
+
 	}
-	return files, nil
+	return fileNames, nil
+}
+
+func generateGIF(fileNames []string, outPath string) error {
+	anim := gif.GIF{LoopCount: len(fileNames)}
+	for _, fileName := range fileNames {
+		reader, err := os.Open(fileName)
+		if err != nil {
+			panic(err)
+		}
+		defer reader.Close()
+		img, _, err := image.Decode(reader)
+		if err != nil {
+			return err
+		}
+		bounds := img.Bounds()
+		drawer := draw.FloydSteinberg
+		palettedImg := image.NewPaletted(bounds, palette.Plan9)
+		drawer.Draw(palettedImg, bounds, img, image.ZP)
+		anim.Image = append(anim.Image, palettedImg)
+		anim.Delay = append(anim.Delay, delay)
+	}
+	file, err := os.Create(outPath)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	encodeErr := gif.EncodeAll(file, &anim)
+	if encodeErr != nil {
+		return encodeErr
+	}
+	return nil
+}
+
+func formatSupported(fileName string) bool {
+	return strings.HasSuffix(fileName, ".png") ||
+		strings.HasSuffix(fileName, ".jpg") ||
+		strings.HasSuffix(fileName, ".jpeg")
+}
+
+func showUsage() {
+	fmt.Println("Usage: gipher <folder paths> <output path>")
 }
